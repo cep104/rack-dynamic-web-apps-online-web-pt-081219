76b891cce7c5fb2a94d5ff23bc5dd89effb8d464
Candice Peters <candicelizabethpeters@gmail.com>
2019-11-19 03:18:59 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,6 +6,11 @@ class Application
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
     resp.write "Hello, World"
+    if num_1==num_2 && num_2==num_3
+      puts "You Win"
+    else
+       puts "You Lose"
+    end
     resp.finish
   end
 
